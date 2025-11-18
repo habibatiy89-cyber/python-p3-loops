aebd724e83c6653cf95d736d9b90ba59981943ff
habiba hussein <habiba.hussein@moringaschool.comclear>
2025-11-18 18:37:11 +0300

    using the for loop in python

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for year in range(10, 0, -1):
+        print(year)    
+    print("Happy New Year!")
+    
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    for i in range(len(int_list)):
+        int_list[i] = int_list[i] ** 2
+    return int_list
+    
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
